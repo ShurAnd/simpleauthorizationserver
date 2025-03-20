b56a59fcf0a6e7795f9d987dfbe12c0b0450c713
Shura <Shura@Shura>
2025-03-20 16:29:36 +0300

    add client registered repository

--- src/main/java/org/andrey/simpleauthorizationserver/config/SecurityUsersConfig.java
+++ src/main/java/org/andrey/simpleauthorizationserver/config/SecurityUsersConfig.java
@@ -10,6 +10,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+
+import java.util.UUID;
 
 /**
  * Класс для конфигурации UserDetailsService и PasswordEncoder для аутентикации пользователей
@@ -41,6 +49,23 @@ public class SecurityUsersConfig {
         }
 
         return userDetailsService;
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(){
+        RegisteredClient registeredClient = RegisteredClient
+                .withId(UUID.randomUUID().toString())
+                .clientId("custom client")
+                .clientSecret("custom client secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://127.0.0.1:9090/authorized")
+                .scope(OidcScopes.OPENID)
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
     }
 
 
