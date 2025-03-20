0032b833c01623cd2b2c09a4026161809f2316ff
Shura <Shura@Shura>
2025-03-20 10:32:03 +0300

    init commit

---
+++ src/main/java/org/andrey/simpleauthorizationserver/SimpleauthorizationserverApplication.java
@@ -0,0 +1,13 @@
+package org.andrey.simpleauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleauthorizationserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleauthorizationserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/andrey/simpleauthorizationserver/SimpleauthorizationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andrey.simpleauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleauthorizationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
